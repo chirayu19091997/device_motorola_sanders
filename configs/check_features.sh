5141215c365bd2eed9065acc7bf4ab99a2c4e5d7
Chirayu Agrawal <Chirayu.agrawal4000@gmail.com>
2019-06-22 09:35:31 -0700

    fix check_features.sh

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -10,22 +10,22 @@ sku=`getprop ro.boot.hardware.sku`
 
 if [ "$sku" = "XT1806" ]; then
     # XT1806 doesn't have NFC chip
-    rm /vendor/etc/permissions/android.hardware.nfc.xml
-    rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
-    rm /vendor/etc/permissions/com.android.nfc_extras.xml
+    rm /system/vendor/etc/permissions/android.hardware.nfc.xml
+    rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
 fi
 
 if ! [ "$sku" = "XT1802" ]; then
     # Others variants doesn't have DTV support
-    rm /vendor/etc/permissions/com.motorola.hardware.dtv.xml
-    rm /vendor/etc/permissions/mot_dtv_permissions.xml
-    rm /vendor/lib/libdtvtuner.so
-    rm /vendor/lib64/libdtvtuner.so
-    rm /vendor/lib/libdtvhal.so
-    rm /vendor/lib64/libdtvhal.so
-    rm -r /vendor/app/DTVPlayer
-    rm -r /vendor/app/DTVService
+    rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
+    rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
+    rm /system/vendor/lib/libdtvtuner.so
+    rm /system/vendor/lib64/libdtvtuner.so
+    rm /system/vendor/lib/libdtvhal.so
+    rm /system/vendor/lib64/libdtvhal.so
+    rm -r /system/app/DTVPlayer
+    rm -r /system/priv-app/DTVService
 fi
 
 if [ "$sku" = "XT1804" ]; then
